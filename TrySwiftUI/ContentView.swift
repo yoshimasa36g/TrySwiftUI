920238e63236f8c76141c2c3212189b0b025dd4c
yoshimasa36g <ashihana785@gmail.com>
2019-11-14 22:26:22 +0900

    :seedling: create project

---
+++ TrySwiftUI/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  TrySwiftUI
+//
+//  Created by Yoshimasa Aoki on 2019/11/14.
+//  Copyright © 2019 yoshimasa36g. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
