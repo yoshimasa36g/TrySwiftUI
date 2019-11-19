f4afbb73f6daf7e0b1f4d5a47694d9a30d0afed3
yoshimasa36g <ashihana785@gmail.com>
2019-11-19 22:55:18 +0900

    Section 7, Pass Data into Child Views

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+        }
+        .navigationBarTitle(Text("Landmarks"))
     }
 }
 
