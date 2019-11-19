133a7731c654c96d73b9737bea542d21ced8de16
yoshimasa36g <ashihana785@gmail.com>
2019-11-19 22:35:48 +0900

    Section 5, Make the List Dynamic

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Yoshimasa Aoki on 2019/11/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
